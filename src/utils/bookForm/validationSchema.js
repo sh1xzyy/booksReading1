905af2412aa4271d907fed66da865c2743fc0e60
sh1xzyy <alekslungol@gmail.com>
2025-06-01 18:00:28 +0300

    .

--- src/utils/bookForm/validationSchema.js
+++ src/utils/bookForm/validationSchema.js
@@ -3,8 +3,6 @@ import * as Yup from 'yup'
 export const validationSchema = Yup.object().shape({
 	title: Yup.string().required('Fill Name'),
 	author: Yup.string().required('Fill Author'),
-	publishYear: Yup.string()
-		.min(1000, 'Not less than 1000!')
-		.required('Fill Publish Year'),
+	publishYear: Yup.string().required('Fill Publish Year'),
 	pagesTotal: Yup.string().required('Fill Pages Total'),
 })
